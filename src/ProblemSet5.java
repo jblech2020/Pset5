12ebb9d190e77b9e7fa32602bdd8d7b4dafc283d
Jaedan Blechinger-Slocum <jblech2020@gmail.com>
2019-10-28 09:03:24 -0400

    Excercise 10

--- src/ProblemSet5.java
+++ src/ProblemSet5.java
@@ -1,128 +1,163 @@
 /**
  * Problem Set 5.
- * 
+ *
  * It's time to put your skills to the test. This problem set focuses on using
  * built-in methods, most notable of the String class. It is also your first
  * introduction to methods that accept parameters and return values. The main
  * method is declared, but it is up to you to implement. Write your code for each
  * exercise in the corresponding method. Make sure you test your code thoroughly.
- * 
+ *
  * The specifications for each exercise are outlined below. Your job is to write
  * lines of code to produce answers to my questions. Each exercise requires that
  * you ask the user to enter one or more values. Your code must meet the
  * requirements set forth in this section.
- * 
+ *
  * Work through these exercises on your own. Experiment, make mistakes, ask
  * questions, and fix your mistakes. It's the only way to get good at programming.
  */
 
 public class ProblemSet5 {
-    
+
     public static void main(String[] args) {
-        
+      ProblemSet5 ps = new ProblemSet5();
+
+      System.out.println(ps.intertwine("", "")); //Excercise 9
+      // System.out.println(ps.isPalindrome("racecar")); //Excercise 10
+
     }
-    
+
     /*
      * Exercise 1.
-     * 
+     *
      * Given two strings, return a new string built by surrounding in with the first
      * and last two characters of out.
      */
-    
+
     public String surroundMe(String in, String out) {
 
+      return "";
     }
-    
+
     /*
      * Exercise 2.
-     * 
+     *
      * Given a string and an integer, return a new string that represents the first
      * and last n characters of text.
      */
-    
+
     public String endsMeet(String text, int n) {
 
+      return "";
     }
-    
+
     /*
      * Exercise 3.
-     * 
+     *
      * Given a string, return a new string using the middle three characters of text.
      */
-    
+
     public String middleMan(String text) {
 
+      return "";
     }
-    
+
     /*
      * Exercise 4.
-     * 
+     *
      * Given two strings, determine whether or not target is equivalent to the middle
      * three characters of text.
      */
-    
+
     public boolean isCentered(String text, String target) {
 
+      return false;
     }
-    
+
     /*
      * Exercise 5.
-     * 
+     *
      * Given a string and a character, compute the number of words that end in suffix.
      */
-    
+
     public int countMe(String text, char suffix) {
 
+      return 0;
     }
-    
+
     /*
      * Exercise 6.
-     * 
+     *
      * Given a string, compute the number of triplets in text.
      */
-    
+
     public int triplets(String text) {
 
+      return 0;
     }
-    
+
     /*
      * Exercise 7.
-     * 
+     *
      * Given a string, compute the sum of the digits in text.
      */
-    
+
     public long addMe(String text) {
 
+      return 0;
     }
-    
+
     /*
      * Exercise 8.
-     * 
+     *
      * Given a string, compute the length of the longest sequence.
      */
-    
+
     public long sequence(String text) {
 
+      return 0;
     }
-    
+
     /*
      * Exercise 9.
-     * 
+     *
      * Given two strings, return a new string built by intertwining each of the
      * characters of a and b.
      */
-    
+
     public String intertwine(String a, String b) {
 
+      return "";
     }
-    
+
     /*
      * Exercise 10.
-     * 
+     *
      * Given a string, determine whether or not it is a palindrome.
      */
-    
-    public boolean isPalindrome(String text) {
 
+    public boolean isPalindrome(String text) {
+      //Initializing Variables
+      String originPal = text;
+      originPal = originPal.toUpperCase();
+      String palTestString = "";
+
+      if (originPal == null){
+        return false;
+      }
+
+      //This code gets rid of spaces
+      for (int i = 0; i<originPal.length(); i++){
+        if (originPal.substring(i, i+1).equals(" ")){
+          originPal = originPal.substring(0, i) + originPal.substring(i+1, originPal.length());
+        }
+      }
+
+      //This code flips the original suspected palindrome in a new string
+      for (int i=originPal.length()-1; i>=0; i--){
+        palTestString = palTestString + originPal.charAt(i);
+      }
+
+      boolean palTest = palTestString.equals(originPal);
+      return palTest;
     }
 }
